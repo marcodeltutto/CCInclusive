1b93f1e0b8b9e96588bcc87847f85264faa4276c
Christoph Rudolf von Rohr <rudolf@lhep.unibe.ch>
2016-04-23 09:32:43 +0200

    Offbeam run with POT information

--- runOnMCC7_numuCC_QE_wnumuvtx.C
+++ runOnMCC7_numuCC_QE_wnumuvtx.C
@@ -51,8 +51,8 @@ int runOnMCC7_numuCC_QE_wnumuvtx()
 //     string GeneratorName = "prodcosmics_corsika_inTime";
 //     string GeneratorName = "data_bnb";
 //     string GeneratorName = "data_bnb_external";
-    string GeneratorName = "data_onbeam_bnb";
-//     string GeneratorName = "data_offbeam_bnbext";
+//     string GeneratorName = "data_onbeam_bnb";
+    string GeneratorName = "data_offbeam_bnbext";
 
     // Initialize and fill track reco product names
     std::vector<string> TrackProdNameVec;
@@ -112,8 +112,8 @@ int runOnMCC7_numuCC_QE_wnumuvtx()
     TChain *treenc = new TChain("analysistree/anatree");
 //     treenc -> Add( ("/lheppc46/data/uBData/anatrees/"+GeneratorName+"_"+Version+"_anatree.root").c_str() );
 //     treenc -> Add( ("/media/christoph/200EFBDA63AA160B/anatrees/"+GeneratorName+"_"+Version+"_anatree.root").c_str() );
-    treenc -> Add( ("/pnfs/uboone/persistent/users/aschu/onbeam_data_bnbSWtrigger/"+GeneratorName+"_"+Version+"_anatree.root").c_str() );
-//     treenc -> Add( ("/pnfs/uboone/persistent/users/aschu/offbeam_data_bnbSWtrigger/"+GeneratorName+"_"+Version+"_anatree.root").c_str() );
+//     treenc -> Add( ("/pnfs/uboone/persistent/users/aschu/onbeam_data_bnbSWtrigger/"+GeneratorName+"_"+Version+"_anatree.root").c_str() );
+    treenc -> Add( ("/pnfs/uboone/persistent/users/aschu/offbeam_data_bnbSWtrigger/"+GeneratorName+"_"+Version+"_anatree.root").c_str() );
 
     //maximum array sizes
     const int maxentries = 35000;
